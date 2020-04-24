5e67ba7010ffb617bfdc9468c92df020e3343b99
Franco Viola <fviol001@fiu.edu>
2020-04-23 23:14:21 -0400

    Found a string that was ambiguous.

--- Problem Set 3/P2/Program.fs
+++ Problem Set 3/P2/Program.fs
@@ -1,4 +1,7 @@
 ﻿(*
 a.  Describe the strings that the CFG recognizes.
 The grammar recognizes binary strings, since they only have 0s and 1s.
+
+b.  This language is ambiguous. Find a string that is recognized by this grammar which has two derivations.
+001101
 *)
