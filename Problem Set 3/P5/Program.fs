df13535bd7253226c630a77748a7736a2972857a
Franco Viola <fviol001@fiu.edu>
2020-04-23 23:28:59 -0400

    Wrote recursive interleave to compare with rail-recursive. Tail-recursion is slower in this case.

--- Problem Set 3/P5/Program.fs
+++ Problem Set 3/P5/Program.fs
@@ -3,3 +3,19 @@
     |[], [] -> list
     |x::xs, y::ys -> let newlist = list @ [x;y]
                      iltr newlist xs ys
+
+let rec il xs ys =
+    match xs, ys with
+    |[], [] -> []
+    |x::xs, y::ys -> x::y::(il xs ys)
+
+let list1 = [1..2..19999]
+let list2 = [2..3..20000]
+
+let test = il list1 list2
+let test2 = iltr [] list1 list2
+
+printf "%A\n\n" test
+printf "%A\n\n" test2
+
+//Tail-recursion is actually slower in this case..
