f47df6d7cb83bf1a11ef8cc580ab77bc20f23c3e
Franco Viola <fviol001@fiu.edu>
2020-04-23 23:32:27 -0400

    Wrote to filter out so only 5-15 are shown.

--- Problem Set 3/P6/Program.fs
+++ Problem Set 3/P6/Program.fs
@@ -4,3 +4,7 @@
         1.0 / (2.0**n * (if ((index + 1) % 2 = 0) then -1.0 else 1.0)))
 
 printfn "%A" seqInfinite
+
+let seq5to15 = 
+    Seq.init 11 (fun index -> 1.0 / (2.0**(float(index) + 5.0)* (if ((index + 1) % 2 = 0) then -1.0 else 1.0)))
+Seq.iter (fun elem -> printf "%f " elem) seq5to15
