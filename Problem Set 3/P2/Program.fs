6b5d6fc6194dc5f655e080ec3c2316b7b8465aef
Franco Viola <fviol001@fiu.edu>
2020-04-23 23:15:24 -0400

    Drew the trees for the ambiguous string.

--- Problem Set 3/P2/Program.fs
+++ Problem Set 3/P2/Program.fs
@@ -4,4 +4,19 @@ The grammar recognizes binary strings, since they only have 0s and 1s.
 
 b.  This language is ambiguous. Find a string that is recognized by this grammar which has two derivations.
 001101
+
+c.  Show the two derivation trees for the string in part (b).
+        S                                       S
+       / \                                     / \
+      0   A                                   0   A
+        / | \                                   / | \
+       0  A  A                                 0  A  A
+         / \  \                                   |  |
+        1   S  \                                  1  | 
+           / \  \                                   / \
+          1   B  \                                 1   S
+              |   \                                   / \
+              0    1                                 0   A
+                                                         |
+                                                         1
 *)
