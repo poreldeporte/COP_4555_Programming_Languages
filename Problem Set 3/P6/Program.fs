069b1019c37c937697031f187d5ed4f3847d648a
Franco Viola <fviol001@fiu.edu>
2020-04-23 23:32:07 -0400

    Wrote alternating infinite sequence.

---
+++ Problem Set 3/P6/Program.fs
@@ -0,0 +1,6 @@
+﻿let seqInfinite =
+    Seq.initInfinite (fun index ->
+        let n = float (index + 1)
+        1.0 / (2.0**n * (if ((index + 1) % 2 = 0) then -1.0 else 1.0)))
+
+printfn "%A" seqInfinite
