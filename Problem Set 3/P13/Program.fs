d3d5813cd6967284281144d6674d358569933448
Franco Viola <fviol001@fiu.edu>
2020-04-25 13:53:33 -0400

    Wrote stack with functions.

--- Problem Set 3/P13/Program.fs
+++ Problem Set 3/P13/Program.fs
@@ -1,2 +1,9 @@
 ﻿type Stack = {Push: int -> int list; Pop: unit -> int list; Top: unit -> int; isEmpty: unit -> bool}
 
+let stack =
+    let stk = ref [2;3;4]
+    {Push = fun x -> x :: (!stk); 
+     Pop = fun () -> stk := List.tail (!stk); 
+     Top = fun () -> List.head (!stk); 
+     isEmpty = fun () -> List.isEmpty(!stk);
+    }
