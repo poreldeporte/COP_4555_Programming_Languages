62394a2a756fcd5a633e32a076fa2f177a50fce4
Franco Viola <fviol001@fiu.edu>
2020-04-25 14:21:13 -0400

    Wrote functions to convert seconds to other units.

--- Problem Set 3/P18/Program.fs
+++ Problem Set 3/P18/Program.fs
@@ -6,3 +6,10 @@
 let sinms = 0.001<s/ms>
 let sinµs = 0.000001<s/µs>
 let sinns = 0.000000001<s/ns>
+
+let constoms (x : float<s>) = (x / sinms)
+//Type:(x:float<s> -> float<ms>)
+let constoµs (x : float<s>) = (x / sinµs)
+//Type:(x:float<s> -> float<µs>)
+let constons (x : float<s>) = (x / sinns)
+//Type:(x:float<s> -> float<ns>)
