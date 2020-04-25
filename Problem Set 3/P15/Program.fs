ab9032478138587074b6dd5ae19d70f9c534b928
Franco Viola <fviol001@fiu.edu>
2020-04-25 14:03:28 -0400

    Created monitored function.

---
+++ Problem Set 3/P15/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let makeMonitoredFun f =
+    let c = ref 0
+    (fun x -> c := !c+1; printf "Called %d times.\n" !c; f x)
+
+let msqrt = makeMonitoredFun sqrt
+
+(*
+First, explain why F# does not allow the following declaration:
+  let mrev = makeMonitoredFun List.rev
+F# does not allow the declaration because mrev has been inferred to have a generic type of
+('_a list -> '_a list) -> ('_a list -> '_a list) as opposed to ('a -> 'b) -> ('a -> 'b)
+
+Now suppose we rewrite the declaration using the technique of eta expansion:
+
+  let mrev = fun x -> (makeMonitoredFun List.rev) x
+
+Does this solve the problem? Explain why or why not. 
+Yes, as it fixes the inference problem since it was specified by the programmer.
+*)
