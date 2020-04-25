32df3889fcfe046348447a0c5bfcfed22a8ccc5e
Franco Viola <fviol001@fiu.edu>
2020-04-24 22:58:32 -0400

    Finished defining function.

--- Problem Set 3/P11/Program.fs
+++ Problem Set 3/P11/Program.fs
@@ -3,4 +3,12 @@
     let f = ref 1
     let temp = ref 0
     let cnt = ref 2
-    
+    if (n <= 1) then 
+        f := n
+    else 
+        while (!cnt <= n) do
+            temp := !f
+            f := !f + !fp
+            fp := !temp
+            cnt := !cnt + 1
+    !f
