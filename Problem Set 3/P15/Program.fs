30a06189543199fd084028c09a09f327d1acf1bb
Franco Viola <fviol001@fiu.edu>
2020-04-25 14:05:14 -0400

    Second declaration of mrev works.

--- Problem Set 3/P15/Program.fs
+++ Problem Set 3/P15/Program.fs
@@ -8,7 +8,10 @@ let msqrt = makeMonitoredFun sqrt
 
 let mrev = makeMonitoredFun List.rev
 
+let mrev = fun x -> (makeMonitoredFun List.rev) x
 (*
 Mrev is inferred to have a generic type of
 ('_a list -> '_a list) -> ('_a list -> '_a list) instead of ('a -> 'b) -> ('a -> 'b)
+
+Second declaration fixes the inference problem since it was specified.
 *)
