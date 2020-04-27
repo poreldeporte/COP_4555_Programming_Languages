fc5a969c887af1a644ecc6d75335ad488942a062
Franco Viola <fviol001@fiu.edu>
2020-04-26 22:35:45 -0400

    Wrote type for tree.

--- Problem Set 3 Part 2/P1/Q13/P1Q13/Program.fs
+++ Problem Set 3 Part 2/P1/Q13/P1Q13/Program.fs
@@ -1,2 +1,11 @@
 ﻿type TERMINAL = IF|THEN|ELSE|BEGIN|END|PRINT|SEMICOLON|ID|EOF
 
+type 'a tree =
+    | Lf
+    | Br of ('a * 'a tree * 'a tree)
+    | Br_IF of ('a tree * 'a tree * 'a tree)
+    | Br_BEGIN of ('a tree * 'a tree)
+    | Br_PRINT of ('a tree)
+    | Br_END of ('a tree)
+    | Br_SEMICOLON of ('a tree * 'a tree)
+    | Br_ID of ('a tree)
