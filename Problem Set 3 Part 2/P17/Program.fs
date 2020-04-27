b292d711b3370f61a8c302aa7f30cf54ba01a74f
Franco Viola <fviol001@fiu.edu>
2020-04-27 14:02:22 -0400

    Wrote conversion functions from inches to unit.

--- Problem Set 3 Part 2/P17/Program.fs
+++ Problem Set 3 Part 2/P17/Program.fs
@@ -4,3 +4,8 @@
 
 let ininft = 12.0<inches/ft>
 let inincm = 0.393701<inches/cm>
+
+let coninchestoft (x : float<inches>) = (x / ininft)
+//Type:(x:float<inches> -> float<ft>)
+let coninchestocm (x : float<inches>) = (x / inincm)
+//Type:(x:float<inches> -> float<cm>)
