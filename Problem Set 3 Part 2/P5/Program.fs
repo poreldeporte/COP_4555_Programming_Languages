2c81b4a61561fce077ac60d28d3f281b8c8ed440
Franco Viola <fviol001@fiu.edu>
2020-04-26 22:46:33 -0400

    Attempted second type deriviation.

--- Problem Set 3 Part 2/P5/Program.fs
+++ Problem Set 3 Part 2/P5/Program.fs
@@ -11,4 +11,17 @@ E |- *p : int* var
 E |- e1 : t var   E |- e2 : t
 -----------------------------
 E |- e1 = e2 : t
+
+Perform a type derivation to prove that x = x + 7 is well typed.
+E(p) = int var
+----------------- (ID)
+E |- p : int var 
+----------------- (R-VAL)  
+E |- p : int 
+----------------- (L-VAL)
+E |- *p : int* var
+
+E |- e1 : t var   E |- e2 : t
+-----------------------------
+E |- e1 = e2 : t
 *)
