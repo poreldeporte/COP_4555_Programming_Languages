50fd61120025432eeea9ace0aa61d93e3a2181e1
Franco Viola <fviol001@fiu.edu>
2020-04-27 14:07:15 -0400

    Tested functions and found values of both examples from problem.

--- Problem Set 3 Part 2/P15/Program.fs
+++ Problem Set 3 Part 2/P15/Program.fs
@@ -4,3 +4,14 @@ let twice f = f << f
 
 let successor n = n+1
 
+let test = thrice twice successor 0
+
+printf "%A" test
+
+(*
+Value of twice thrice successor 0 will be 9
+1+1+1=3 ; 3+3+3=9
+
+Value of thrice twice successor 0 will be 8
+1+1=2 ; 2+2=4 ; 4+4=8
+*)
