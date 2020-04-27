a75974364cd23f2f1f22c57fa4ccd7de41200c6f
Franco Viola <fviol001@fiu.edu>
2020-04-26 22:46:15 -0400

    Wrote the first type deriviation.

---
+++ Problem Set 3 Part 2/P5/Program.fs
@@ -0,0 +1,14 @@
+﻿(*
+Perform a type derivation to prove that x = 7 is well typed.
+E(p) = int var
+----------------- (ID)
+E |- p : int var 
+----------------- (R-VAL)  
+E |- p : int 
+----------------- (L-VAL)
+E |- *p : int* var
+
+E |- e1 : t var   E |- e2 : t
+-----------------------------
+E |- e1 = e2 : t
+*)
