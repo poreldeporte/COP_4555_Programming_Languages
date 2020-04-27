1fc92e76ee2ffd51d7bc0614a80794205d4d41ff
Franco Viola <fviol001@fiu.edu>
2020-04-26 22:32:44 -0400

    Wrote e production rule.

--- Problem Set 3 Part 2/P1/Q4/P1Q4/Program.fs
+++ Problem Set 3 Part 2/P1/Q4/P1Q4/Program.fs
@@ -6,3 +6,10 @@ let error = "Compiled unsuccessfully"
 
 let advance t = List.tail t
 
+let rec e xs =
+    let newlist = t xs
+    match List.head newlist with
+    |ADD -> advance newlist |> e 
+    |SUB -> advance newlist |> e
+    |_ -> newlist
+
