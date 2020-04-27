eec3c83deadad8307da01a01ce5782d640e60514
Franco Viola <fviol001@fiu.edu>
2020-04-26 22:29:54 -0400

    Wrote eat terminal.

--- Problem Set 3 Part 2/P1/Q3/P1Q3/Program.fs
+++ Problem Set 3 Part 2/P1/Q3/P1Q3/Program.fs
@@ -3,3 +3,10 @@
 let accept = "Compiled successfully"
 
 let error = "Compiled unsuccessfully"
+
+let eat terminal = function
+| [] -> failwith "Empty token on eat"
+| x::xs -> if x = terminal then xs else failwith "Not a valid token for eat"
+
+let e = eat ID
+
