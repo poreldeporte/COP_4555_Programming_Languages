b02ca2cb04cffb99948a92daccfde2e0484fc958
Franco Viola <fviol001@fiu.edu>
2020-04-28 19:46:03 -0400

    Finished writing the multipleSum function.

--- Problem Set 3 Part 2/P8/Program.fs
+++ Problem Set 3 Part 2/P8/Program.fs
@@ -3,3 +3,6 @@
     |0 | 1 | 2 -> value
     |x -> if x % 3 <> 0 then 
             multipleSum value (x - (x % 3))
+          else 
+            let newValue = value + x
+            multipleSum newValue (x - 3)
