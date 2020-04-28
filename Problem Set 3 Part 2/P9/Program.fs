7c7c604a657c967541546ca8d0ca73c4e298c6f9
Franco Viola <fviol001@fiu.edu>
2020-04-28 19:48:57 -0400

    Finished writing function.

--- Problem Set 3 Part 2/P9/Program.fs
+++ Problem Set 3 Part 2/P9/Program.fs
@@ -3,3 +3,6 @@
     |0 -> value
     |_ -> if x % counter <> 0 then 
             divisorSum (counter - 1) value x
+          else 
+            let newValue = value + counter
+            divisorSum (counter - 1) newValue x
